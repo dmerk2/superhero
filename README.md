d3760d7889a59ee6696dc3e63b4f2ee8783f5529
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-11-24 19:43:05 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # superhero
+# super-hero
--- assets/js.js
+++ assets/js.js
@@ -2,4 +2,20 @@
 
 $(document).ready(function(){
 	//alert("Hi");
+	var nameChoice = prompt("What would you like to name your super hero?")
+
+	$("#superHero").html(nameChoice);
+	$("#jumboP").html("Choose what power " + nameChoice + " should use!");
+
+
+//change size, color, opacity....
+
+	$(".visButton").on("click", function(){
+		$(".superMan").animate({opacity: "1"});
+	});
+
+	$(".invisButton").on("click", function(){
+		$(".superMan").animate({opacity: "0.1"});
+	});
+
 });
